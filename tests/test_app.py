c92675008a6635f77cd6ccf3742037a08f3d0dcc
Diego Go <diego.gomes87@gmail.com>
2025-08-02 17:40:50 +0200

    Add test for client

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,25 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Bora pra mais uma'}
+
+
+def test_create_user(client):
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': '123ABC',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    data = response.json()
+    assert data['username'] == 'alice'
+    assert data['email'] == 'alice@example.com'
+    assert 'id' in data
