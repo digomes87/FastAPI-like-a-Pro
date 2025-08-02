7a1b994e5ddb41405d73d8354649c54a380f201d
Diego Go <diego.gomes87@gmail.com>
2025-08-02 17:40:50 +0200

    Add test for client

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', status_code=200)
+
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Bora pra mais uma'}
+    return {"message": "Bora pra mais uma"}
+
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
