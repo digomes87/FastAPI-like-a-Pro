1a52d390381f13ec76c0664823b462687a1a1c80
Diego Go <diego.gomes87@gmail.com>
2025-08-02 18:03:39 +0200

    Add test for create user

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,25 +1,26 @@
 from http import HTTPStatus
 
+from fastapi.testclient import TestClient
 
-def test_root_deve_retornar_ok_e_ola_mundo(client):
-    response = client.get('/')
 
+def test_root_deve_retornar_ok_e_ola_mundo(client: TestClient) -> None:
+    response = client.get("/")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Bora pra mais uma'}
+    assert response.json() == {"message": "Bora pra mais uma"}
 
 
-def test_create_user(client):
+def test_create_user(client: TestClient) -> None:
 
     response = client.post(
-        '/users/',
+        "/users/",
         json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': '123ABC',
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "123ABC",
         },
     )
     assert response.status_code == HTTPStatus.CREATED
     data = response.json()
-    assert data['username'] == 'alice'
-    assert data['email'] == 'alice@example.com'
-    assert 'id' in data
+    assert data["username"] == "alice"
+    assert data["email"] == "alice@example.com"
+    assert "id" in data
